57a731809260ceb854450d5a6c2cb21265c2bdef
Joao Vitor Dalla Villa <joaodvilla@gmail.com>
2021-06-05 16:39:17 -0300

    test auth

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import 'dotenv/config';
 import { server } from './http';
 import "./websocket/GameService";
 
-server.listen(process.env.PORT || process.env.PORT_LOCAL, () => console.log("Running!"));
+server.listen(process.env.PORT || process.env.PORT_LOCAL, () => console.log("Running!"))
